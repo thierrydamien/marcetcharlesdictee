ca29bd07a4e1179c25ab0d7d5b526b2eef4d8d19
thierrydamien <thierrydamien@gmail.com>
2026-06-14 10:43:32 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Le jeu des devoirs - service worker */
-const CACHE = 'hg-pwa-v98';
+const CACHE = 'hg-pwa-v99';
 const SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon.svg','./apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
